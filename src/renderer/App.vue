81039290a6e0901473bbe6d105fd46c74ceda30f
w4ngzhen <w4ngzhen@hotmail.com>
2020-10-10 15:03:12 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ele-im'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
